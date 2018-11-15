8b3e5a0305a8dcaf73b2f35e91ffb01b13666697
Cameron Kay <ckay@uoregon.edu>
2018-11-15 11:28:26 -0800

    I couldn't figure out where l.vowels was coming from, but I assume it was NNK.xls. I imported it using rio::import and here::here.

--- Heidi Jungah Jeongim-Final project codes.R
+++ Heidi Jungah Jeongim-Final project codes.R
@@ -16,7 +16,9 @@ library(tidyverse)###CAM: I moved the loading of tidyverse up here so all of the
 ####Section 1. The following functions are generated to meet the requirement of EDLD 610 final peoject (we used sumamrize() above).####
 
 #l.vowels is the main dataframe we will work on, we rename it into df
-View(l.vowels)
+View(l.vowels) ###CAM: I can't seem to find where the l.vowels object is coming from. Are you importing NK.xls or NNK.xls? I've added the code below to load NNK.xls
+l.vowels <- import(here("data", "NNK.xls"))
+
 df <- l.vowels ###CAM: I added spaces around the assignment operator for legibility. I've also added spaces around assignment operators and equal signs in the rest of the document. It is just a stylistic thing. In some cases, there was a space between `filter` and the first paranthesis of the function. I removed those as well. Rstudio has a handy tool under `RStudio > Preferences > Code > Diagnostics > Provide R Style Diagnostics (e.g., Whitespace)`, which will atuomatically flag whitespace issues for you :)
 View(df)
 
@@ -26,7 +28,7 @@ View(td)
 
 ####gather####
 gtd <- td %>% 
-  gather(Forments,Value, -c(1:2,5))
+  gather(Forments, Value, -c(1:2,5))
 View(gtd)   #works
 
 ####separate####
